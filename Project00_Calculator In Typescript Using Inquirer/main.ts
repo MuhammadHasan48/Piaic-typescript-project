30d4275df26d40c51f00707559d46a29e91b98c1
MuhammadHasan48 <muhammadhasan4823@gmail.com>
2024-01-02 23:12:28 +0500

    typescript project

---
+++ Project00_Calculator In Typescript Using Inquirer/main.ts
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+
+const answers =await inquirer.prompt([
+    {
+        type: "number",
+        name: "number1",
+        message: "please enter your first number:"
+
+    },
+    { 
+        type: "number",
+        name: "number2",
+        message: "please enter your second number:"
+
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices:["addition","subtraction","multiplication","division"],
+        message: "please select operator:"
+
+    }
+
+])
+
+const {number1,number2,operator}=answers;
+
+if(number1 && number2 && operator){
+    let result:number=0;
+    if(operator === "addition"){
+        result = number1+number2;
+    }
+    else if(operator === "subtraction"){
+        result = number1-number2;
+    }
+    else if(operator === "multiplication"){
+        result = number1*number2;
+    }
+    else if(operator === "division"){
+        result = number1/number2;
+    }
+    console.log("your result is",result);
+
+}else{
+    console.log("kindly enter valid input");
+    
+}
+
